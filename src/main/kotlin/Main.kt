5e6f2419b9a9181501e3c6040a3393ccc8f7761a
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-04-07 05:52:11 -0300

    When Expression fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,17 @@
 package com.harukadev
 
-import kotlin.system.exitProcess
-
 fun main() {
     print("Please enter a number: ")
     val input = readln()
     val inputAsInt = input.toIntOrNull()
 
-        val output = when(inputAsInt) {
-            null-> "Dude, enter a valid number!"
+    if (inputAsInt !== null) {
+        val output = when {
             inputAsInt % 2 == 0 -> "The number is even"
             inputAsInt < 10 -> "The number is odd and less than 10"
-            else ->"The number is odd and least 11"
+            else -> "The number is odd and least 11"
         }
 
         println(output)
+    } else println("Dude, enter a valid number!")
 }
