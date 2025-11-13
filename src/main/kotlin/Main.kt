6d5a70eab2cc8b5101a46b382097c67c145a0a62
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-11-13 08:54:55 -0300

    step: Extension functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,22 @@ package com.harukadev
 import kotlin.system.exitProcess
 
 fun main() {
-    println("Hello World")
+    while (true) {
+        print("Type the text: ")
+        val input = readln()
+
+        if (input.isEmpty() || input.length < 2) continue
+
+        val inputReversed = input.shuffled()
+        println("Shuffled text: $inputReversed")
+        break
+    }
     exitProcess(0)
+}
+
+/**
+ * Returns a new string where the characters of the original string are shuffled randomly.
+ */
+fun String.shuffled(): String {
+    return this.toList().shuffled().joinToString("")
 }
