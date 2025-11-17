437c77353acf9c83ee9b4d232f7a91929ee8f091
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-11-17 09:20:06 -0300

    step: normal class

---
+++ src/main/kotlin/Classes.kt
@@ -0,0 +1,34 @@
+package com.harukadev
+
+import kotlin.math.PI
+import kotlin.math.sqrt
+import kotlin.system.exitProcess
+
+fun main() {
+    val rect1 = Rectangle(width = 5f, height = 7f)
+    val rect2 = rect1.copy(height = 10f)
+
+    println(rect1)
+    println("The diagonal is ${rect1.diagonal} and area is ${rect1.area}")
+    println("Max area is ${maxArea(rect1, rect2)}")
+    println("Are rectangles Rect1 and Rect2 equals? ${rect1 == rect2}")
+
+    val circle = Circle(radius = 10f)
+    println("The area of the circle is ${circle.area} and the diagonal is ${circle.diagonal}")
+
+    exitProcess(0)
+}
+
+fun maxArea(firstRectangle: Rectangle, secondRectangle: Rectangle): Float {
+    return maxOf(firstRectangle.area, secondRectangle.area)
+}
+
+data class Rectangle(val width: Float, val height: Float) {
+    val diagonal = sqrt(width * width + height * height)
+    val area = width * height
+}
+
+data class Circle(val radius: Float) {
+    val area = radius * radius * PI
+    val diagonal = area * radius
+}
